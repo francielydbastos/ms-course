c0501187434a135f3b4d944c32ad3551b654eeab
Fran Bastos <francielydbastos@gmail.com>
2024-11-03 18:05:49 -0300

    rest template implementation

--- hr-payroll/src/main/java/com/francielydbastos/hr_payroll/services/PaymentService.java
+++ hr-payroll/src/main/java/com/francielydbastos/hr_payroll/services/PaymentService.java
@@ -1,12 +1,31 @@
 package com.francielydbastos.hr_payroll.services;
 
 import com.francielydbastos.hr_payroll.entities.Payment;
+import com.francielydbastos.hr_payroll.entities.Worker;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @Service
 public class PaymentService {
 
+    @Autowired
+    private RestTemplate restTemplate;
+
+    @Value("${hr-worker.host}")
+    private String workerHost;
+
     public Payment getPayment(Long workerId, int days) {
-        return new Payment("Nome", 200.0, days);
+
+        Map<String, String> uriVariables = new HashMap<>();
+        uriVariables.put("id", String.valueOf(workerId));
+
+        Worker worker = restTemplate.getForObject(workerHost+"/workers/{id}", Worker.class, uriVariables);
+
+        return new Payment(worker.getName(), worker.getDailyIncome(), days);
     }
 }
